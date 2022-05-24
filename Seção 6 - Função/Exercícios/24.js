00def5e728d1152f5550b5e859149accb2e099c3
AMSJRBR <alexandremedeirosjunior@gmail.com>
2022-05-23 22:33:34 -0300

    Exercitando

--- Seção 6 - Função/Exercícios/24.js
+++ Seção 6 - Função/Exercícios/24.js
@@ -2,9 +2,13 @@
  * 24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
  */
 
-let contador = 0  
 
-while (contador < 11) {
-    contador++;
-    console.log("Hello World" + contador)
+helloWorld = () => {
+    let contador = 0
+    while (contador < 11) {
+        console.log("Hello World")
+        contador++;
+    }
 }
+
+helloWorld()
