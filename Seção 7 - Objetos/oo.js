804ce0535cf012b0a31b52549f73cfc2be7ce711
AMSJRBR <alexandremedeirosjunior@gmail.com>
2022-05-24 21:03:04 -0300

    Conceitos de OO

---
+++ Seção 7 - Objetos/oo.js
@@ -0,0 +1,24 @@
+// CÓDIGO NÃO EXECUTÁVEL!!! 
+
+// Conceitos de paradigma
+
+// Procendural
+processamento(valor1, valor2, valor3)
+
+// OO - Orientação ao Objeto
+objeto = {
+    valor1,
+    valor2,
+    valor,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Principios principais
+// 1.Abstracao
+// 2.Encapsulamento
+// 3.Herança (Prototype)
+// 4.Polimorfismo
