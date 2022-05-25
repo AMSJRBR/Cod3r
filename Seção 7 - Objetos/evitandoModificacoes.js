eb51b84c08898ef40f74354451cd03de057f846a
AMSJRBR <alexandremedeirosjunior@gmail.com>
2022-05-25 15:14:37 -0300

    Seal, Extentios and Freeze

---
+++ Seção 7 - Objetos/evitandoModificacoes.js
@@ -0,0 +1,22 @@
+// Object.preventExtensions - Não deixa acrescentar mais atributos, podemos modificar e excluir
+const produto = Object.preventExtensions({
+    nome: 'Qualquer', preco: 1.99, tag: 'promoção'
+})
+console.log('Extensível', Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.descricao = 'Borracha escolar branca'
+delete produto.tag
+console.log(produto)
+
+// Object.seal - Não consegue adicionar e remover atributos, so alterar os existentes
+const pessoa = { nome: 'Juliana', idade: 35 }
+Object.seal(pessoa)
+console.log('Selado', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+console.log(pessoa)
+
+//Object.freeze = selado + valores constantes
