a4a548c97177ea7f4eca58ee4a1741e4ded10227
AMSJRBR <alexandremedeirosjunior@gmail.com>
2022-06-02 00:00:57 -0300

    Multiplicacao sem *

--- Seção 9 - Exercícios/8.js
+++ Seção 9 - Exercícios/8.js
@@ -1,6 +1,12 @@
 /* Desenvolva uma função que recebe dois números inteiros não negativos (maiores ou iguais a zero) e realize a
 multiplicação deles. Porém, não utilize o operador de mutiplicação. */
 
-mult = (x, y) => {
-    
-}
+function multiplicar(numeroA, numeroB) {
+    let resultado = 0
+    for(let i = 0; i < numeroB; i++)
+    resultado += numeroA
+    return resultado
+    }
+
+console.log(multiplicar(2,5))
+console.log(multiplicar(5,2))  
